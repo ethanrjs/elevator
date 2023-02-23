45e3b71d79e72e0ef0d444754a132e336865cd6c
Ethan <ethan.rettinger@greatoaks.com>
2023-02-22 23:00:59 -0500

    Ding™️

---
+++ background.js
@@ -0,0 +1,21 @@
+const audio = new Audio(browser.runtime.getURL('elevator.mp3'));
+audio.loop = true;
+
+// when page fully loads
+browser.webNavigation.onCompleted.addListener(details => {
+    // pause audio when page fully loads
+    if (details.frameId === 0) {
+        audio.pause();
+        const ding = new Audio(browser.runtime.getURL('ding.mp3'));
+        ding.play();
+    }
+});
+
+// play audio when page navigation starts
+browser.webNavigation.onBeforeNavigate.addListener(details => {
+    if (details.frameId === 0) {
+        // restart audio
+        audio.currentTime = 0.1;
+        audio.play();
+    }
+});
